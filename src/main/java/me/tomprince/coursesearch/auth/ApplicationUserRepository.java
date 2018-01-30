f4fbc135ab0b29a9bdc0e086f57a7bb054198bd9
Tom Prince <tprince14@gmail.com>
2018-01-30 14:38:36 -0500

    finish up jwt

---
+++ src/main/java/me/tomprince/coursesearch/auth/ApplicationUserRepository.java
@@ -0,0 +1,7 @@
+package me.tomprince.coursesearch.auth;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ApplicationUserRepository extends JpaRepository<ApplicationUser, Long> {
+    ApplicationUser findByUsername(String username);
+}
